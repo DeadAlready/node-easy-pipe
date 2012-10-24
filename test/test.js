87890d0824c1d5ae7218e6225d843bd68a78cc42
Karl Dyyna <karl.dyyna@gmail.com>
2012-10-24 16:45:05 +0300

    Allow streams to be passed as well as child processes bump version to 0.0.2

--- test/test.js
+++ test/test.js
@@ -2,11 +2,13 @@
  * Copyright 2012 Karl Düüna <karl.dyyna@gmail.com> All rights reserved.
  */
 'use strict';
-
+var fs = require('fs');
 var assert = require('assert');
 var $p = require('../index');
 var s = require('child_process').spawn('node',['test1.js']);
+var writeS = fs.createWriteStream('test.log');
 
-var a = $p(s).pipe('node test2.js').on('data',function(d){
+var a = $p(s).pipe(writeS).pipe('node test2.js').on('data',function(d){
   assert.equal('data: hello', d.toString('utf8'));
+  assert.equal('hello',fs.readFileSync('test.log','utf8'));
 });
